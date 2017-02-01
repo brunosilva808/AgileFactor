7cf2dc20d0f24cf71697a05c747ff6b85681fe3c
Bruno Silva <brunosilva@nunos-mac-mini.sinfic.pt>
2017-02-01 11:17:48 +0000

    Commit before passcode

---
+++ Agile Factor/PinCodeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PinCodeViewController.swift
+//  Agile Factor
+//
+//  Created by Bruno Silva on 01/02/2017.
+//  Copyright © 2017 Bruno Silva. All rights reserved.
+//
+
+import UIKit
+
+class PinCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
